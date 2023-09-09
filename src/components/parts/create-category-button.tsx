03b944ba9aa8faad0e0a12fba4f549b8011422f8
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2023-09-09 17:31:30 +0900

    ユーザー操作をメニューにまとめる

--- src/components/parts/create-category-button.tsx
+++ src/components/parts/create-category-button.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@tremor/react";
 import { useCallback, useState } from "react";
+import { FolderPlusIcon } from "@heroicons/react/20/solid";
 import { CreateCategoryDialog } from "./create-category-dialog";
 
 export function CreateCategoryButton() {
@@ -9,9 +9,13 @@ export function CreateCategoryButton() {
 
   return (
     <>
-      <Button size="xs" variant="secondary" onClick={() => openDialog()}>
+      <button
+        className="group         flex w-full items-center rounded-md px-2 py-2 text-sm text-gray-900 hover:bg-blue-500 hover:text-white"
+        onClick={() => openDialog()}
+      >
+        <FolderPlusIcon className="mr-2 h-5 w-5" aria-hidden="true" />
         カテゴリ作成
-      </Button>
+      </button>
       <CreateCategoryDialog isOpen={isOpenDialog} handleClose={closeDialog} />
     </>
   );
